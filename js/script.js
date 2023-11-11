5653801aa99d7b6564875de1752d35765f8cbe46
Adam Epinger <76474223+Maskedfoxguy@users.noreply.github.com>
2023-11-11 13:43:14 +0100

    idfixing\

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ window.onload = function () {
     const restartButton = document.getElementById("restart-button");
     let game;
   
-    startButton.addEventListener("click", function () {
-      startGame();
-    });
-  
     function startGame() {
-      game = new Game(); 
-  
-      game.start();
+      console.log("start game");
+      game = new Game(); // create an instance of the Game class
+      game.start(); // execute the start method
     }
-  };
+  
+    function restartGame() {
+      location.reload();
+    }
+}
