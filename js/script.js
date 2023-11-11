10fbd905d9f8c5b9282e52b0ebaf49ca23f619e2
Jennifer Manella <jennifermanella@MacBook-Pro-de-Jennifer.local>
2023-11-11 15:24:14 +0100

    show

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@ window.onload = function () {
     const restartButton = document.getElementById("restart-button");
     let game;
   
-    function startGame() {
-      console.log("start game");
-      game = new Game(); // create an instance of the Game class
-      game.start(); // execute the start method
-    }
+    startButton.addEventListener("click", function () {
+      startGame();
+    });
   
-    function restartGame() {
-      location.reload();
+    function startGame() {
+      game = new Game(); 
+      game.start();
     }
-}
+  }
+ 
