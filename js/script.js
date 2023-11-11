738b87073a6c6cdc34853dd7f3573f235d384de4
Adam Epinger <76474223+Maskedfoxguy@users.noreply.github.com>
2023-11-11 14:22:31 +0100

    PoolCHeck

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
 window.onload = function () {
-    const startButton = document.getElementById("start-button");
-    const restartButton = document.getElementById("restart-button");
-    let game;
-  
-    function startGame() {
-      console.log("start game");
-      game = new Game(); // create an instance of the Game class
-      game.start(); // execute the start method
-    }
-  
-    function restartGame() {
-      location.reload();
-    }
+  const startButton = document.getElementById("start-button");
+  const restartButton = document.getElementById("restart-button");
+  let game;
+  startButton.addEventListener("click", function () {
+    startGame();
+  });
+  function startGame() {
+    game = new Game();
+    game.start();
+  }
 }
