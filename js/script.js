3b5537cfdb9e26643274bd4fd0d9ce3e54b2e630
Jennifer Manella <jennifermanella@MacBook-Pro-de-Jennifer.local>
2023-11-11 18:07:46 +0100

    link

--- js/script.js
+++ js/script.js
@@ -1,17 +1,55 @@
 window.onload = function () {
-    const startButton = document.getElementById("start-button");
-    const restartButton = document.getElementById("restart-button");
-    let game;
-  
-    startButton.addEventListener("click", function () {
-      startGame();
-    });
-  
-    function startGame() {
-      game = new Game(); 
-      game.start();
-    }
+  const startButton = document.getElementById("start-button");
+  const restartButton = document.getElementById("restart-button");
+  let game;
 
-    
+
+  function startGame() {
+    console.log("start game");
+    game = new Game(); 
+    game.start(); 
   }
+
+  function restartGame() {
+    location.reload();
+  }
+
+  function handleKeydown(event) {
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
+
+ 
+    if (possibleKeystrokes.includes(key)) {
+      event.preventDefault();
+
+
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -1;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+          break;
+      }
+    }
+  }
+
+  startButton.addEventListener("click", function () {
+    startGame();
+  });
+
+
+   window.addEventListener("keydown", handleKeydown);
+};
  
