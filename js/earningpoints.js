f905b7343bf6ec384bc9e54dfef55634ca2a75aa
Jennifer Manella <jennifermanella@MacBook-Pro-de-Jennifer.local>
2023-11-13 18:18:15 +0100

    gamescreen style

--- js/earningpoints.js
+++ js/earningpoints.js
@@ -1,12 +1,15 @@
-class Obstacle {
+class Earningpoints {
     constructor(gameScreen){
 this.gameScreen = gameScreen;
 this.top= Math.floor(Math.random() * 150 + 40);
 this.rigth =0;
 this.width = 150;
 this.height = 150;
-this.element = document.createElement('img');
-this.element.src = ''
+// let languages = ['CSS','HTML','JAVASCRIPT'];
+// let randomposition = (Math.random()* languages.length)
+// let language = languages [randomposition]
+//this.element = document.createElement(<p></p>);
+//this.element.innerHtml = <p>${language}</p>;
 this.element.style.position = 'absolute';
 this.element.style.width = `${this.width}px`;
 this.element.style.height = `${this.height}px`;
